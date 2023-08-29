9f833cc52e8754e4df6dd9650358ddd22ef19a61
Prince Gerald Asilo <asilo.prince25@gmail.com>
2023-08-29 20:15:00 +0800

    refactor: clean up component code

--- client/src/components/SideBar.tsx
+++ client/src/components/SideBar.tsx
@@ -1,26 +1,12 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import Header from "./Header";
-import {
-  FaPeopleArrows,
-  FaUserFriends,
-  FaPiggyBank,
-  FaRegChartBar,
-  FaChartLine,
-  FaCaretRight,
-  FaCaretDown,
-  FaCog,
-  FaFileAlt,
-  FaBars,
-  FaTimes,
-  FaCalendarCheck,
-  FaEdit,
-} from "react-icons/fa";
+import links from "../utils/link";
+import { FaBars, FaCog, FaTimes } from "react-icons/fa";
 
 function SideBar() {
-  // for mobile navigation
+  // for mobile navigation responsiveness
   const [sideBar, setSideBar] = useState(false);
-  const [subButton, setSubButton] = useState(false);
 
   const navigationHandler = () => {
     setSideBar(!sideBar);
@@ -33,128 +19,34 @@ function SideBar() {
           sideBar ? "-left-0" : "-left-full"
         }  h-full col-span-1 p-8 border-r`}
       >
-        <div className="text-center pb-2">
+        <div className="text-center pb-4">
           <h1 className="font-poppin font-bold text-2xl uppercase tracking-[4px]">
             Cityvet
           </h1>
         </div>
-        <div className="flex flex-col justify-between h-[550px]">
+        <div className="flex flex-col justify-between h-[530px]">
           {/* navigation sidebar */}
           <nav>
             <ul>
-              <p className="text-xs text-gray-500 pt-4">Main</p>
-              <li>
-                <Link
-                  to="/"
-                  className="flex items-center gap-4 hover:bg-cyan-600 p-2 hover:text-white text-sm text-gray-700 font-semibold rounded-lg transition-colors ml-1"
-                >
-                  <FaRegChartBar /> Dashboard
-                </Link>
-              </li>
-              <div className="h-16 mb-5">
-                <li>
-                  <button
-                    className="flex items-center gap-4 w-40  text-sm text-gray-700 font-semibold transition-all rounded-lg transition-colors "
-                    onClick={() => {
-                      setSubButton(!subButton);
-                    }}
-                  >
-                    <div className="flex items-center">
-                      {subButton ? (
-                        <FaCaretDown className="text-xs" />
-                      ) : (
-                        <FaCaretRight className="text-xs" />
-                      )}
-                      <FaPeopleArrows />
-                    </div>
-                    Dispersal Program
-                  </button>
-                  {/* output subButton if true */}
-                  {subButton && (
-                    <ul className="absolute py-2 space-y-2 text-xs  overflow-y-auto ">
-                      <li className="flex items-center">
-                        <Link
-                          to="disperse"
-                          className="flex items-center justify-center align-middle ml-0.5 p-2 text-gray-700 w-44 font-semibold hover:bg-cyan-600 hover:text-white transition-colors"
-                        >
-                          Disperse
-                        </Link>
-                      </li>
-                      <li className="flex items-center">
-                        <Link
-                          to="#"
-                          className="flex items-center justify-center align-middle ml-2 p-2 text-gray-700 w-44 font-semibold hover:bg-cyan-600 hover:text-white transition-colors"
-                        >
-                          Redisperse
-                        </Link>
-                      </li>
-                    </ul>
-                  )}
-                </li>
-              </div>
-
-              <p className="text-xs text-gray-500 pt-4">List</p>
-
-              <li>
-                <Link
-                  to="/benefeciaries"
-                  className="flex items-center gap-4 ml-1 hover:bg-cyan-600 p-2 hover:text-white text-sm text-gray-700 font-semibold rounded-lg transition-colors"
-                >
-                  <FaUserFriends /> Recipients
-                </Link>
-              </li>
-              <li>
-                <Link
-                  to="/livestocks"
-                  className="flex items-center gap-4 ml-1 hover:bg-cyan-600 p-2 hover:text-white text-sm text-gray-700 font-semibold rounded-lg transition-colors"
-                >
-                  <FaPiggyBank />
-                  Livestock
-                </Link>
-              </li>
-              <p className="text-xs text-gray-500 pt-4">General</p>
-
-              <li>
-                <Link
-                  to="/benefeciaries"
-                  className="flex items-center gap-4 ml-1 hover:bg-cyan-600 p-2 hover:text-white text-sm text-gray-700 font-semibold rounded-lg transition-colors"
-                >
-                  <FaEdit /> Notes
-                </Link>
-              </li>
-              <li>
-                <Link
-                  to="/livestocks"
-                  className="flex items-center gap-4 ml-1 hover:bg-cyan-600 p-2 hover:text-white text-sm text-gray-700 font-semibold rounded-lg transition-colors"
-                >
-                  <FaCalendarCheck />
-                  Calendar
-                </Link>
-              </li>
+              {links.map((link) => (
+                <div key={link.id}>
+                  <p className="text-xs text-gray-500 pt-4">{link.title}</p>
+                  {link.listLinks.map((listLink) => (
+                    <li key={listLink.id}>
+                      <Link
+                        to={listLink.url}
+                        className="flex items-center gap-4 hover:bg-cyan-600 p-2 hover:text-white text-sm text-gray-700 font-semibold rounded-lg transition-colors ml-1"
+                      >
+                        {listLink.icon}
+                        {listLink.title}
+                      </Link>
+                    </li>
+                  ))}
+                </div>
+              ))}
             </ul>
           </nav>
-          {/* analytics */}
-          <div>
-            <p className="text-xs text-gray-500 p-2">Analytics</p>
-            <ul>
-              <li>
-                <Link
-                  to="/"
-                  className="flex items-center gap-4 ml-1 hover:bg-cyan-600 p-2 hover:text-white text-sm text-gray-700 font-semibold rounded-lg transition-colors"
-                >
-                  <FaChartLine /> Statistics
-                </Link>
-              </li>
-              <li>
-                <Link
-                  to="/benefeciaries"
-                  className="flex items-center gap-4 ml-1 hover:bg-cyan-600 p-2 hover:text-white text-sm text-gray-700 font-semibold rounded-lg transition-colors"
-                >
-                  <FaFileAlt /> Logs
-                </Link>
-              </li>
-            </ul>
-          </div>
+
           {/* logout */}
           <div className="flex flex-col gap-4">
             <Link
---
+++ client/src/utils/link.tsx
@@ -0,0 +1,87 @@
+import {
+  FaPeopleArrows,
+  FaUserFriends,
+  FaPiggyBank,
+  FaRegChartBar,
+  FaChartLine,
+  FaFileAlt,
+  FaCalendarCheck,
+  FaEdit,
+} from "react-icons/fa";
+
+const links = [
+  {
+    id: 1,
+    title: "Main",
+    listLinks: [
+      {
+        id: 1,
+        title: "Dashboard",
+        url: "/",
+        icon: <FaRegChartBar />,
+      },
+      {
+        id: 2,
+        title: "Dispersal",
+        url: "/",
+        icon: <FaPeopleArrows />,
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "List",
+    listLinks: [
+      {
+        id: 1,
+        title: "Livestock",
+        url: "/livestocks",
+        icon: <FaPiggyBank />,
+      },
+      {
+        id: 2,
+        title: "Benefeciaries",
+        url: "/benefeciaries",
+        icon: <FaUserFriends />,
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: "General",
+    listLinks: [
+      {
+        id: 1,
+        title: "Notes",
+        url: "/notes",
+        icon: <FaEdit />,
+      },
+      {
+        id: 2,
+        title: "Calendar",
+        url: "/calendar",
+        icon: <FaCalendarCheck />,
+      },
+    ],
+  },
+  {
+    id: 4,
+    title: "Analytics",
+    listLinks: [
+      {
+        id: 1,
+        title: "Statistics",
+        url: "/statistics",
+        icon: <FaChartLine />,
+      },
+      {
+        id: 2,
+        title: "Logs",
+        url: "/logs",
+        icon: <FaFileAlt />,
+      },
+    ],
+  },
+];
+
+export default links;
