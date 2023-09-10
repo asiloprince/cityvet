213e58958a150553b7f6d941d50a42772c63fb05
Prince Gerald Asilo <asilo.prince25@gmail.com>
2023-09-10 17:16:09 +0800

    Configure dev and prod environments and server structure

--- server/index.js
+++ server/index.js
@@ -1,12 +1,20 @@
 import express from "express";
 import dotenv from "dotenv";
-
-dotenv.config();
 const app = express();
 
+// configurations
+const envPath = `./.env.${process.env.NODE_ENV}`;
+dotenv.config({ path: envPath });
+
+// API
 app.get("/", (req, res) => {
   res.json({ message: "hello world" });
 });
+
+// serve for production
+if (process.env.NODE_ENV === "production") {
+  console.log("app is running on production");
+}
 
 app.listen(process.env.PORT, () => {
   console.log(`Server Listening to ${process.env.PORT}`);
