92d19853ef84b8ec3929a7b1e8435ad87b264850
Prince Gerald Asilo <asilo.prince25@gmail.com>
2023-09-12 12:06:42 +0800

    add dialogs components

---
+++ client/src/components/data-table/data-table-row-actions.tsx
@@ -0,0 +1,12 @@
+// import { Row } from "@tanstack/react-table";
+
+// import { Button } from "../ui/button";
+// import { Dialog, DialogContent, DialogTrigger } from "../ui/dialog";
+// import {
+//   DropdownMenu,
+//   DropdownMenuContent,
+//   DropdownMenuItem,
+//   DropdownMenuLabel,
+//   DropdownMenuSeparator,
+//   DropdownMenuTrigger,
+// } from "../ui/dropdown-menu";
