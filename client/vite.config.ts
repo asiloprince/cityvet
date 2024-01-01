b10b3632590f7e709225a29fe1ab03a06356204b
asiloprince <asilo.prince25@gmail.com>
2024-01-01 23:56:06 +0800

    feat: pwa configuration with vitePWA plugin

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,28 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import { VitePWA, VitePWAOptions } from "vite-plugin-pwa";
+import react from "@vitejs/plugin-react";
+
+const manifestForPlugin: Partial<VitePWAOptions> = {
+  registerType: "prompt",
+  manifest: {
+    name: "Cityvet",
+    short_name: "Cityvet",
+    start_url: ".",
+    display: "standalone",
+    background_color: "#fff",
+    description:
+      "Cityvet Livestock Dispersal Program Livestock Management System.",
+    icons: [
+      {
+        src: "/logo.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+    ],
+  },
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), VitePWA(manifestForPlugin)],
+});
